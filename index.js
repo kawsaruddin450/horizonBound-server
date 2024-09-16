b9a057c31cbef8f8cc9b9aea422606b2668c4c5b
kawsar-uddin <kawsaruddin450@gmail.com>
2024-09-16 12:43:50 +0600

    connect to database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const cors = require('cors');
 
@@ -7,11 +8,39 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send(`HorizonBound Server is running at: ${port}`);
 })
 
 
-app.listen(port, ()=> {
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.acntg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
     console.log("Camp HorizonBound Server is running at:", port);
 })
